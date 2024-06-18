9af89975dd4a1bd6460ba51614db663a6d2fd0cf
asliceofpizza007 <a89884526@hotmail.com.tw>
2024-06-18 17:24:05 +0800

    [chore] adjust content-wrapper class

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,8 +2,8 @@ import Breadcrumbs from "./Breadcrumbs";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 flex items-center bg-au-background border-b border-b-[#ffffff1a] border-solid h-10 xl:h-14">
-      <div className="container">
+    <header className="sticky top-0 flex items-center bg-au-background border-b border-b-[#ffffff1a] border-solid h-10 z-50 xl:h-14">
+      <div className="content-wrapper">
         <Breadcrumbs />
       </div>
     </header>
