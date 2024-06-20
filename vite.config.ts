3b18700439bd29cb45eadd6c0f8ff186779b60de
asliceofpizza007 <a89884526@hotmail.com.tw>
2024-06-20 15:21:19 +0800

    [chore] adjust base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,4 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // base url match the project name for github page.
-  return {
-    plugins: [react(), TanStackRouterVite()],
-    base: mode === "development" ? "/" : "/hahow-fe-pretest",
-  };
-});
+export default defineConfig({ plugins: [react(), TanStackRouterVite()] });
